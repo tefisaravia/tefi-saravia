3d58b589bccc98430a9c4bd8f1ef909e921f8ebe
tefi_saravia <tefisaraviavirtual@gmail.com>
2020-10-05 18:46:51 -0600

    added a back to top button

---
+++ js/main.js
@@ -0,0 +1,33 @@
+
+
+
+
+////////////////// Back to top button  //////////////////
+var amountScrolled = 300;
+
+$(document).ready(function(){
+
+    //fade in back to top button
+    $(window).scroll(function() {
+        if ( $(window).scrollTop() > amountScrolled ) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+
+    //scroll animation back to top
+    $('.back-to-top').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 700);
+        return false;
+    });
+
+
+
+
+});
+
+
+
